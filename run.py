7a52c9bb5032bf9c24db4d24cfe475f9ca20860e
Liam O'Connor <liamoconnor8481@gmail.com>
2019-07-16 10:42:32 +0100

    addedd chat textbox functionality and refactored code

--- run.py
+++ run.py
@@ -1,34 +1,63 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the messages list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    ## messages_dict = {"timestamp": now, "from": username, "message": message}
+    # removed dict above an put directly to append. One line of code better than two!
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-def get_all_messages():
-    """Get all of the messages and seperate using a <br>"""
-    return "<br>".join(messages)
+##### def get_all_messages():
+####     """Get all of the messages and seperate using a <br>"""
+###     return "<br>".join(messages)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
 
-@app.route('/<username>')
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    
+    if "username" in session:
+        ## return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+# we can add the /chat/ bit once we have used url_for
+@app.route('/chat/<username>', methods = ["GET", "POST"])
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    """Add and Display chat messages"""
+    ## return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    """Create a new message and redirect to the chat page"""
-    # return "{0}: {1}".format(username, message)
-    add_messages(username, message)
-    return redirect("/" + username)
+    # if a message has been added we want to add it to the messages list
+    if request.method == "POST":
+        # if POST, then we will obtain some variables 
+        username = session["username"]
+        # message object come from our form so we use the request method
+        message = request.form["message"]
+        # call add_messages function with variables just created to add them to the list
+        add_message(username, message)
+        # redirect to session username or messages will continue forever
+        ## return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username = username, chat_messages = messages)
+
+"""we started with this, but can be removed now because we're using a text box"""
+# @app.route('/<username>/<message>')
+# def send_message(username, message):
+#     """Create a new message and redirect to the chat page"""
+#     ## return "{0}: {1}".format(username, message)
+#     add_message(username, message)
+#     return redirect("/" + username)
 
 app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
